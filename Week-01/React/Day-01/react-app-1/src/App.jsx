6b177cc33356e3cc97bae1277caddfdd98a2e5f3
Arzoo Jain <arzoo.jain@mindbowser.com>
2025-06-11 18:37:40 +0530

    Basic react app assignment- learning from the videos

---
+++ Week-01/React/Day-01/react-app-1/src/App.jsx
@@ -0,0 +1,17 @@
+function App(){
+  // let message = "Bye bye!";
+
+  // if(Math.random()>0.5){
+  //   message = "Hemlooo";
+  // }
+
+  const name = "Arzoo Jain";
+
+  return(
+    <>
+      <h1>My name is: {name}</h1> 
+    </>
+  )
+}
+
+export default App;
