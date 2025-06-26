81bb3fd7f63294f53bdbf7c93a8447bc3332c335
Arzoo Jain <arzoo.jain@mindbowser.com>
2025-06-26 14:42:52 +0530

    Added btn

--- react/comps-app/src/App.jsx
+++ react/comps-app/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
       <Button type="primary" onClick={()=>console.log("clicked!")}>Click</Button>
       <Button type="danger">Logout</Button>
       <Button type='warning' disabled>Warning</Button>
-      <Button type='success'success outlined>Done</Button>
+      {/* <Button type='success'success outlined>Done</Button> */}
         
 
     </div>
